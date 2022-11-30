eed4c953b827aa5cda75626268512ab8eaeafe2a
Mr. Zoell <raynaldizoel@gmail.com>
2022-11-30 19:24:03 +0700

    Marubah nama button pada Register & Success Register

---
+++ app/src/main/java/binar/academy/kelompok6/tripie_buyer/view/register/RegisterSuccessFragment.kt
@@ -0,0 +1,19 @@
+package binar.academy.kelompok6.tripie_buyer.view.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import binar.academy.kelompok6.tripie_buyer.R
+
+class RegisterSuccessFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register_success, container, false)
+    }
+}
