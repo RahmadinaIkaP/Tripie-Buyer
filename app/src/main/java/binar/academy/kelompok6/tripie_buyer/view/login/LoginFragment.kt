7ca11648e9127dc95df6d842780fed9db2bf0b14
aghniqisthi <aghniqisthi@gmail.com>
2022-11-30 16:21:37 +0700

    commit layout login

--- app/src/main/java/binar/academy/kelompok6/tripie_buyer/view/login/LoginFragment.kt
+++ app/src/main/java/binar/academy/kelompok6/tripie_buyer/view/login/LoginFragment.kt
@@ -1,32 +1,27 @@
-package binar.academy.kelompok6.tripie_buyer.view.splashscreen
+package binar.academy.kelompok6.tripie_buyer.view.login
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
 import binar.academy.kelompok6.tripie_buyer.R
+import binar.academy.kelompok6.tripie_buyer.databinding.FragmentLoginBinding
 import binar.academy.kelompok6.tripie_buyer.databinding.FragmentSplashScreenBinding
 
-class SplashScreenFragment : Fragment() {
-    private var _binding : FragmentSplashScreenBinding? = null
+class LoginFragment : Fragment() {
+
+    private var _binding : FragmentLoginBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        startSplashScreen()
-    }
-
-    private fun startSplashScreen() {
     }
 }
