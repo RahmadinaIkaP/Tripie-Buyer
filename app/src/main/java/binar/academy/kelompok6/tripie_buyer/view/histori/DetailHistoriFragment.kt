3e2edabad6d8e3da7f92bdfd7ed95d8d986a9de4
Mr. Zoell <raynaldizoel@gmail.com>
2022-12-01 22:19:24 +0700

    Add History & Detail History

--- app/src/main/java/binar/academy/kelompok6/tripie_buyer/view/histori/DetailHistoriFragment.kt
+++ app/src/main/java/binar/academy/kelompok6/tripie_buyer/view/histori/DetailHistoriFragment.kt
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import binar.academy.kelompok6.tripie_buyer.R
 
-class HistoriFragment : Fragment() {
+class DetailHistoriFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_histori, container, false)
+        return inflater.inflate(R.layout.fragment_detail_histori, container, false)
     }
 }
