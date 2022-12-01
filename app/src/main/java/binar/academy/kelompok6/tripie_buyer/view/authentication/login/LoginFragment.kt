d18531ea52ecb6d8648338ae9f82a85189789199
aghniqisthi <aghniqisthi@gmail.com>
2022-12-01 15:42:55 +0700

    commit layout profile

--- app/src/main/java/binar/academy/kelompok6/tripie_buyer/view/authentication/login/LoginFragment.kt
+++ app/src/main/java/binar/academy/kelompok6/tripie_buyer/view/authentication/login/LoginFragment.kt
@@ -1,25 +1,47 @@
 package binar.academy.kelompok6.tripie_buyer.view.authentication.login
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
+import binar.academy.kelompok6.tripie_buyer.R
 import binar.academy.kelompok6.tripie_buyer.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
-    private var _binding : FragmentLoginBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentLoginBinding
+    private lateinit var sharedPref: SharedPreferences
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.btnMasuk.setOnClickListener {
+            //username dan password di login
+            val inputEmail = binding.editTextEmail.text.toString()
+            val inputPass = binding.editTextPass.text.toString()
+
+            if(inputEmail == "" || inputPass == ""){
+                Toast.makeText(context, "Fill In Username and Password!", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                //seleksi data yg diinput dengan data di API
+                //ada -> navigasi ke home, gada -> toast no data
+            }
+        }
+
+        binding.btnBuatAkun.setOnClickListener {
+            //masuk fragment register
+            Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_profileFragment)
+        }
     }
 }
--- app/src/main/java/binar/academy/kelompok6/tripie_buyer/view/home/HomeFragment.kt
+++ app/src/main/java/binar/academy/kelompok6/tripie_buyer/view/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import binar.academy.kelompok6.tripie_buyer.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -36,6 +37,11 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
     companion object {
