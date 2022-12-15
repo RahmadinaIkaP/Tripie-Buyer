340e28be8cf8e68e637d44ae1bc8da53b2448ec0
Mr. Zoell <raynaldizoel@gmail.com>
2022-12-15 14:27:43 +0700

    Register Function Post Data to API

--- app/src/main/java/binar/academy/kelompok6/tripie_buyer/data/model/request/RegisterRequest.kt
+++ app/src/main/java/binar/academy/kelompok6/tripie_buyer/data/model/request/RegisterRequest.kt
@@ -4,10 +4,10 @@ package binar.academy.kelompok6.tripie_buyer.data.model.request
 import com.google.gson.annotations.SerializedName
 
 data class RegisterRequest(
-    @SerializedName("email")
-    val email: String,
     @SerializedName("name")
     val name: String,
+    @SerializedName("email")
+    val email: String,
     @SerializedName("password")
     val password: String
 )
